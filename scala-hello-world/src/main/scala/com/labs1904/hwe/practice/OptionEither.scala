e4488343daf41cfa00de3ce885e39a25add013d4
nickhepp <nickhepp@gmail.com>
2023-04-10 23:55:16 -0500

    Working on OptionEither challenges.

--- scala-hello-world/src/main/scala/com/labs1904/hwe/practice/OptionEither.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/practice/OptionEither.scala
@@ -9,18 +9,27 @@ object OptionEither {
     Returns age of a dog when given a human age.
     Returns None if the input is None.
   */
-  def dogAge(humanAge: Option[Int]): Option[Int] = ???
+  def dogAge(humanAge: Option[Int]): Option[Int] = {
+    humanAge.map(ha => ha * 7)
+  }
 
   /*
     Returns the total cost af any item.
     If that item has a price, then the price + 7% of the price should be returned.
   */
-  def totalCost(item: Item): Option[Double] = ???
+  def totalCost(item: Item): Option[Double] = {
+    item.price.map(p => p * 1.07)
+  }
 
   /*
     Given a list of weather temperatures, calculates the average temperature across all weather stations.
     Some weather stations don't report temperature
     Returns None if the list is empty or no weather stations contain any temperature reading.
    */
-  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = ???
+  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = {
+
+    temperatures.a
+
+
+  }
 }
